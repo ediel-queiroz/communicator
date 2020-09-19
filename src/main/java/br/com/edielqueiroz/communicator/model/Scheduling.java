ad78fd9b6d0b678c8c8e2b57f15882670bb6e397
equeiroz <ediel.rp@gmail.com>
2020-09-19 17:28:06 -0300

    Repository creation

--- src/main/java/br/com/edielqueiroz/communicator/model/Scheduling.java
+++ src/main/java/br/com/edielqueiroz/communicator/model/Scheduling.java
@@ -1,12 +1,14 @@
 package br.com.edielqueiroz.communicator.model;
 
 import java.time.LocalDateTime;
+import java.util.Set;
 import java.util.UUID;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 
 @Entity
 public class Scheduling {
@@ -18,6 +20,9 @@ public class Scheduling {
 	private LocalDateTime updatedAt;
 	private LocalDateTime dateTime;
 	private Status status;
+
+	@OneToMany(mappedBy = "scheduling")
+	private Set<Message> messages;
 
 	public enum Status {
 		PENDING, SENT
